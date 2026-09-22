bcf96c8c1e80cea5adc8107180bfd2d59a3c6d9b
Gowthami Gurubilli <2022wc86185@wilp.bits-pilani.ac.in>
2026-09-22 17:59:12 +0530

    Task 4: modified source to trigger continuous integration

--- src/main/java/com/bits/devops/App.java
+++ src/main/java/com/bits/devops/App.java
@@ -8,6 +8,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "DevOps Lab - Continuous Build & Integration - BITS ID: 2022wc86185" );
+        System.out.println( "DevOps Lab - Continuous Build & Integration - BITS ID: 2022wc86185 -TASK 4 - CI Demo" );
     }
 }
+
+
